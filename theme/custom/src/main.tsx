bec0c77f3676075091cd52d9a6b5c0067af80347
brunomarchasson <brunomarchasson@users.noreply.github.com>
2025-06-05 11:26:35 +0200

    Update main.tsx

--- theme/custom/src/main.tsx
+++ theme/custom/src/main.tsx
@@ -2,6 +2,19 @@ import { createRoot } from 'react-dom/client';
 import { Resume } from './Resume';
 import resumeData from './storyResume.json';
 import '../style.css';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translations from './translations.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: translations,
+    lng: 'fr',
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<Resume {...resumeData} />);
