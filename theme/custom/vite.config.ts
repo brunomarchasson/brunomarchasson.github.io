489395529b71ba92a9ddf9fde86343e52e7e8508
brunomarchasson <brunomarchasson@users.noreply.github.com>
2025-06-05 09:16:13 +0200

    Update vite.config.ts

--- theme/custom/vite.config.ts
+++ theme/custom/vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 // import eslint from "vite-plugin-eslint";
+import path from "path";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    // eslint(),
-  ],
-  build: {
-    ssr: true,
-    outDir: "./dist",
-    rollupOptions: {
-      input: "./src/index.tsx",
-      external: ["react", "react-dom/server"],
-      output: {
-        format: "esm",
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+  return {
+    plugins: [
+      react(),
+      // eslint(),
+    ],
+    build: {
+      ssr: true,
+      outDir: "./dist",
+      rollupOptions: {
+        input: path.resolve(__dirname, "src/index.tsx"),
+        external: ["react", "react-dom/server"],
+        output: {
+          format: "esm",
+        },
       },
     },
-  },
-  server: {
-    open: true,
-    fs: {
-      allow: ["..", "."],
+    server: {
+      open: true,
+      fs: {
+        allow: ["..", "."],
+      },
     },
-  },
-  root: './src',
+    root: isBuild ? undefined : './src',
+  };
 });
